7acaca86e291a7e01f348c044df8d330c29e9ecd
Yehia Ibrahim <yibrahim.py@gmail.com>
2020-08-09 05:58:05 +0200

    first commit

--- src/main/java/com/yahya/documentation/SwaggerConfig.java
+++ src/main/java/com/yahya/documentation/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("api.adels.xyz")
+                .host("api.wellwaretech.com")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.yahya"))
                 .build()
