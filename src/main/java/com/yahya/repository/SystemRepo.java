e21f13eb2d48ccf8ea6336e9e2b3016a02cefd48
Yehia Ibrahim <yibrahim.py@gmail.com>
2020-08-16 03:01:17 +0200

    first commit

--- src/main/java/com/yahya/repository/SystemRepo.java
+++ src/main/java/com/yahya/repository/SystemRepo.java
@@ -3,8 +3,10 @@ package com.yahya.repository;
 import com.yahya.entities.System;
 import com.yahya.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.sql.Time;
+import org.springframework.stereotype.Repository;
 
+import java.sql.Time;
+@Repository
 public interface SystemRepo extends JpaRepository<System, Long> {
 
     System findTopByUserOrderByIdDesc(User user);
